7cf39c2f7a427f025ecc412b26e5c9936411f161
boyhagemann <boy.hagemann@komparu.com>
2016-08-02 14:34:41 +0200

    modular colors

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
+//var uncss = require('gulp-uncss');
 
 
 var paths = {
@@ -9,14 +10,16 @@ var paths = {
         library: './src/scss',
         user: './styleguides/**/*.scss',
         destination: './public/styleguides'
-    }
+    },
+    html: ['./public/styleguides/**/*.html']
 }
 
 gulp.task('scss', function() {
-        return gulp.src(paths.scss.user)
-            .pipe(sass({includePaths: [paths.scss.library]}))
-            .pipe(gulp.dest(paths.scss.destination))
-            .pipe(cssmin())
-            .pipe(rename({suffix: '.min'}))
-            .pipe(gulp.dest(paths.scss.destination));
+    return gulp.src(paths.scss.user)
+        .pipe(sass({includePaths: [paths.scss.library]}))
+        //.pipe(uncss({html: paths.html}))
+        .pipe(gulp.dest(paths.scss.destination))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.scss.destination));
 });
